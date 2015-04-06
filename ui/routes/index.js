02c24da796cdee6ce300dd9b64d47ed768162289
Dilyan UZUNOV <uzunov_d@etna-alternance.net>
2015-04-06 20:49:45 +0200

    web

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('index.html', { title: 'Polydesk Homepage' });
+});
+/* GET running desktops. */
+router.get('/running', function(req, res, next) {
+  res.render('running_desk.html', { title: 'Running Polydesk' });
+});
+
+/* GET new desktops. */
+router.get('/new', function(req, res, next) {
+  res.render('new_desk.html', { title: 'New Polydesk' });
 });
 
 module.exports = router;
