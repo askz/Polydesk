60a0cfba927816120bcc2762712848e39306e6f2
Dilyan UZUNOV <uzunov_d@etna-alternance.net>
2015-04-08 10:37:24 +0200

    running js

---
+++ ui/public/js/running_desk.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  function showonlyone(thechosenone) {
+    $("div[name*='infos']").each(function(index) {
+       if ($(this).attr("id") == thechosenone) {
+          $(this).show(0);
+           }
+        else {
+           $(this).hide(0);
+        }
+    });
+  }
+});
