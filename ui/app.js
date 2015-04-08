ea59578c1ffc91c8d26d28ba6064b0f87f06be65
apkaz <admin@example.com>
2015-04-08 10:58:32 +0200

    prepare new desk form for production

--- ui/app.js
+++ ui/app.js
@@ -27,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/desktop', desktop);
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- ui/config.js
+++ ui/config.js
@@ -4,11 +4,6 @@ var Docker = require('dockerode');
 module.exports = {
   socket : process.env.DOCKER_SOCKET || '/var/run/docker.sock',
   repo : 'polydesk/',
-  prefix : "polydesk-",
-  docker : new Docker({socketPath: '/var/run/docker.sock'})
+  prefix : "",
+  docker : new Docker({socketPath: this.socket})
 }
-// var config.
-// var config.
-// var config.
-
-// module.exports = config;
